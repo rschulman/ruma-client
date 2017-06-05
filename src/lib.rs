4c050b0497c8ec47905382a77fe70a8598f314e3
Simon Goller <neosam+github@posteo.de>
2017-06-05 10:50:39 +0200

    Add ability to register as guest.
    Add function to Client which registers as guest and sets the
    its session attribute.

--- src/lib.rs
+++ src/lib.rs
@@ -24,12 +24,15 @@ use hyper::client::HttpConnector;
 use ruma_api::Endpoint;
 use tokio_core::reactor::Handle;
 use url::Url;
+use url::Host;
 
 pub use error::Error;
 pub use session::Session;
 
 mod error;
 mod session;
+
+use ruma_client_api::r0::account::register;
 
 /// A client for the Matrix client-server API.
 #[derive(Debug)]
@@ -64,5 +67,29 @@ impl Client {
             .and_then(
                 |hyper_response| E::Response::future_from(hyper_response).map_err(Error::from),
             )
+    }
+
+    /// Registers as guest
+    pub fn guest_session<'a>(&'a mut self) -> impl Future<Item=(), Error=Error> + 'a {
+        let request_data : register::Request = register::Request {
+            bind_email: None,
+            password: None,
+            username: None,
+            device_id: None,
+            initial_device_display_name: None,
+            auth: None,
+            kind: Some(register::RegistrationKind::Guest)
+        };
+        let response_future = self.request::<register::Endpoint>(request_data);
+        let eval_future = response_future.and_then(move |response : register::Response| {
+            self.session = Some(Session {
+                access_token: response.access_token,
+                homeserver: Host::parse(&response.home_server)?,
+                user_id: response.user_id
+            });
+            let res : Result<(), Error> = Ok(());
+            res 
+        });
+        eval_future
     }
 }
