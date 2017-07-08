e1a50676caa87d4983a4dca4e4b8aca3274c9092
Ross Schulman <ross@rbs.io>
2017-07-08 11:10:59 -0400

    Toying around

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -16,9 +16,10 @@ use ruma_client::Client;
 use ruma_client_api::r0::alias::get_alias;
 use ruma_client_api::r0::membership::join_room_by_id;
 use ruma_client_api::r0::send::send_message_event;
+use ruma_client_api::r0::sync::sync_events;
 use ruma_events::EventType;
 use ruma_events::room::message::{MessageEventContent, MessageType, TextMessageEventContent};
-use ruma_identifiers::RoomAliasId;
+use ruma_identifiers::RoomId;
 use tokio_core::reactor::Core;
 use url::Url;
 
@@ -47,6 +48,28 @@ fn hello_world<'a>(client: &'a Client) -> impl Future<Item = (), Error = ruma_cl
     }).map(|_| ())
 }
 
+fn run(conn: Client) -> impl Future<Item=(), Error=()> {
+    loop {
+        let mut since_time: Option<String> = None;
+        client.request::<sync_events::Endpoint>(sync_events::Request {
+            filter: None,
+            since: since_time,
+            full_state: None,
+            set_presence: None,
+            timeout: None, // Should probably set a timeout...
+        }).and_then(move |response| {
+            since_time = Some(response.next_batch);
+            if response.rooms.invite.len() > 0 {
+                for room in response.rooms.invite.key() {
+                    client.request::<join_room_by_id::Endpoint(join_room_by_id::Request {
+                        room_id: RoomId::try_from()
+                    })
+                }
+            }
+        })
+    }
+}
+
 fn main() {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
@@ -54,6 +77,8 @@ fn main() {
 
     let mut client = Client::new(&handle, server);
 
-    core.run(client.guest_session()).unwrap();
-    core.run(hello_world(&client)).unwrap();
+    core.run(client.login("northship", "thisisapass"))
+    .and_then(run);
+    //core.run(client.guest_session()).unwrap();
+    //core.run(hello_world(&client)).unwrap();
 }
